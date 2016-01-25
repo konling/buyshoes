9a5fe2fbed2c48999ff31f110712f5f84a4f14c0
zhujin <zhujin.nova@gmail.com>
2016-01-25 19:21:48 +0800

    Lession-08

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
- 
+window.onload = function() {
+    console.log("page loaded");
+    makeCartScrollNicely();
+    toggleSidebar();
+}
+
+function makeCartScrollNicely() {
+    var cart = document.querySelector(".cart");
+    Ps.initialize(cart);
+}
+
+function toggleSidebar () {
+    var $toggle = document.querySelector(".site__right-sidebar-toggle");
+    $toggle.addEventListener("click",function() {
+        document.body.classList.toggle("js-show-right-sidebar");
+    });
+}
+
+
